27358318cbb24f3250cab71b610764ab311831dc
Piotr Kawula <kontakt@piotrkawula.pl>
2020-04-06 22:01:34 +0200

    Feat: changed input component

--- src/components/atoms/InputField/InputField.js
+++ src/components/atoms/InputField/InputField.js
@@ -2,20 +2,54 @@ import React from 'react';
 import styled from 'styled-components';
 import propTypes from 'prop-types';
 
+const StyledWrapper = styled.div`
+  width: 100%;
+  display: block;
+  position: relative;
+`;
+
 const StyledInput = styled.input`
   display: block;
   width: 100%;
   font-size: ${({ theme }) => theme.fontSize.s};
   padding: 0.5em;
-  margin: 0.5em auto;
+  margin: 1.5em auto;
   box-shadow: 2px 2px 10px -1px hsla(0, 0%, 0%, 0.2);
-  border: 3px solid ${({ theme }) => theme.primary};
-  border-radius: 0.5em;
+  border: none;
+  border-bottom: 3px solid ${({ theme }) => theme.primary};
+  border-radius: 0.5em 0.5em 0 0;
   background: transparent;
+
+  &:focus ~ span,
+  &:active ~ span,
+  &:not([value='']) ~ span {
+    transform: translate(0em, -0.5em);
+    text-transform: uppercase;
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+    opacity: 1;
+  }
+
+  &:not([value='']) {
+    border-bottom-color: ${({ theme }) => theme.success};
+  }
+`;
+
+const StyledInputName = styled.span`
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translate(0.5em, 2em);
+  font-size: ${({ theme }) => theme.fontSize.s};
+  transition: transform 0.3s ease-in-out;
+  opacity: 0.6;
+  pointer-events: none;
 `;
 
 const InputField = ({ type, name, placeholder, ...props }) => (
-  <StyledInput type={type} {...name} placeholder={placeholder} {...props} />
+  <StyledWrapper>
+    <StyledInput type={type} {...name} {...props} />
+    <StyledInputName>{placeholder}</StyledInputName>
+  </StyledWrapper>
 );
 
 InputField.propTypes = {
