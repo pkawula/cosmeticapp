29080eb66d481d87ed9e2951ef65af0435f6a272
Piotr Kawula <kontakt@piotrkawula.pl>
2020-10-17 20:51:26 +0200

    Hide firebase api keys in .env

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCNRJT5pGAZUNKePDy6vs9uczE0isrQdFA',
-  authDomain: 'cosmeticapp-2e4aa.firebaseapp.com',
-  databaseURL: 'https://cosmeticapp-2e4aa.firebaseio.com',
-  projectId: 'cosmeticapp-2e4aa',
-  storageBucket: 'cosmeticapp-2e4aa.appspot.com',
-  messagingSenderId: '1091715577451',
-  appId: '1:1091715577451:web:0c9d6c6c598c6d52b5632e',
-  measurementId: 'G-5D6B0W6MCL',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
